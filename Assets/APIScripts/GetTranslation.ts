0058919c2aca98517f58bc5dd71234726bef4466
Sanjana Singhania <147664492+sanjana-singhania@users.noreply.github.com>
2024-10-05 23:20:36 -0400

    translation api script (need to fix)

--- Assets/APIScripts/GetTranslation.ts
+++ Assets/APIScripts/GetTranslation.ts
@@ -3,21 +3,28 @@
 export class NewScript extends BaseScriptComponent {
 
     remoteServiceModule = require('LensStudio:RemoteServiceModule');
+    //temp specID: 363ee2a5-ad35-4a5f-9547-d42b2c60a927
     
     @input()
     authKey : string;
     
     onAwake() {
-        this.translateText('Hello good morning', 'ZH-HANS');
+        this.remoteServiceModule.apiSpecId = '363ee2a5-ad35-4a5f-9547-d42b2c60a927';//'http';//'363ee2a5-ad35-4a5f-9547-d42b2c60a927';
+        this.createEvent("TapEvent").bind(() => {
+            this.translateText('Hello', 'ZH-HANS')
+        })
+        //this.translateText('Hello good morning', 'ZH-HANS');
     }
 
-    private translateText(text: string, targetLang: string) : string {
+    translateText(text: string, targetLang: string) {
         const url: string = 'https://api-free.deepl.com/v2/translate';
+        print("api spec id: " + this.remoteServiceModule.apiSpecId) // TODO - REMOVE
 
         const queryParams = `?auth_key=${this.authKey}&text=${encodeURIComponent(text)}&target_lang=${targetLang}`;
 
         let httpRequest = RemoteServiceHttpRequest.create();
         httpRequest.url = url + queryParams;
+        print('url: ' + httpRequest.url) // TODO - REMOVE
         httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
 
         print('Sending translation request!');
@@ -34,7 +41,6 @@ export class NewScript extends BaseScriptComponent {
                 translatedResponse = "an error occurred"
             }
         });
-        return translatedResponse
     }
     
 }
